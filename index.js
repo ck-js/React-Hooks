12d4af1eafe20198ee2d06c08633addfdaeff5cf
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-04-17 15:23:44 +0700

    Add 2 different functional components that utilized use state method. The first component to update the input field and display it using an event handler and use state that was set to empty double quotes. The second component was a click counter that use use state through an event handler and e target value to update the update count variable. There are some minute differences in syntax that i am still unfamiliar with but imn getting there.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
@@ -16,13 +16,47 @@ class App1 extends React.Component {
   }
 }
 
+const App2 = (props) => {
+  const [textValue,textUpdate] =useState("");
 
+  return (
+    <div>
+      <h2>An example...</h2>
+      <label for="name">Whats your name?</label>
+      <input 
+      id="name"
+      type="text"
+      value={textValue}
+      onChange={(e) => {
+        textUpdate(e.target.value)
+      }}
+      placeholder="egDoe Cord"
+      />
+      <p>Powered by React Hooks</p>
+    </div>
+  )
+}
 
-
+const App3 = (props) => {
+  const [count, updateCount] = useState(0);
+  const handleClick = () => {
+    updateCount(count + 1);
+  }
+  return (
+    <div>
+      <h1>I am a functional component with a use state </h1>
+      <p>I have been clicked {count} too many times</p>
+      <button
+      onClick={handleClick}>
+        Anger
+      </button>
+    </div>
+  )
+}
 
 
 ReactDOM.render(
-<App1
+<App3
   />,
   document.getElementById("root")
 )
